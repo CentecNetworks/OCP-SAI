d09f5f4d998dfb49a1b5beba91028baab8a617d7
hezhichuan <hezc@centecnetworks.com>
2018-05-23 15:14:29 +0800

    revert old version

--- inc/sairpfgroup.h
+++ inc/sairpfgroup.h
@@ -114,7 +114,7 @@ typedef enum _sai_rpf_group_member_attr_t
 /**
  * @brief Create RPF interface group
  *
- * @param[inout] rpf_group_id RPF interface group id
+ * @param[out] rpf_group_id RPF interface group id
  * @param[in] switch_id Switch id
  * @param[in] attr_count Number of attributes
  * @param[in] attr_list Array of attributes
@@ -122,7 +122,7 @@ typedef enum _sai_rpf_group_member_attr_t
  * @return #SAI_STATUS_SUCCESS on success, failure status code on error
  */
 typedef sai_status_t (*sai_create_rpf_group_fn)(
-        _InOut_ sai_object_id_t *rpf_group_id,
+        _Out_ sai_object_id_t *rpf_group_id,
         _In_ sai_object_id_t switch_id,
         _In_ uint32_t attr_count,
         _In_ const sai_attribute_t *attr_list);
